f96e13b5eedd7e3c7ad552ea55b91f0395f398ea
Anurag Mishra <doitmywayweb@gmail.com>
2024-07-08 18:26:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const makeCommit = async n => {
     const x = random.int(0, 27);
     const y = random.int(0, 6);
 
-    const DATE = moment().subtract(2, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
+    const DATE = moment().subtract(3, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
 
     const data = {
         date: DATE
